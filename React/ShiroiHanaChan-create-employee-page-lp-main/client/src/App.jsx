8ac55a3f98874f750401994800e08e26dfe3162f
ShiroiHanaChan <170823460+ShiroiHanaChan@users.noreply.github.com>
2026-04-27 22:09:01 +0100

    Fetching data for employee page

--- React/ShiroiHanaChan-create-employee-page-lp-main/client/src/App.jsx
+++ React/ShiroiHanaChan-create-employee-page-lp-main/client/src/App.jsx
@@ -1,14 +1,32 @@
 import { Container } from "@chakra-ui/react";
-
 import { Header } from "./components/Header";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Employee from "./components/Employee";
+
+const queryClient = new QueryClient();
+
+// TODO: Refactor
+export async function fetchEmployees() {
+    const response = await fetch('http://localhost:3030/employees');
+    return response.json();
+}
 
 function App() {
   return (
     <>
-      <Header />
-      <Container pt="6" maxW="container.md">
-        {/* add non-header content here */}
-      </Container>
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Container pt="6" maxW="container.md">
+          {/* add non-header content here */}
+          <Employee />
+        </Container>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
--- React/ShiroiHanaChan-create-employee-page-lp-main/client/src/components/Employee.jsx
+++ React/ShiroiHanaChan-create-employee-page-lp-main/client/src/components/Employee.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { fetchEmployees } from '../App';
+import { useParams } from 'react-router-dom';
 
 export default function Employee() {
-  return 
-      <>
-        
-      </>
+
+  const { data, status } = useQuery(
+    { queryKey: ['employees'], queryFn: fetchEmployees, }
+  );
+
+  let { employeeId } = useParams();
+
+  return (
+    <>
+      <section aria-labelledby='employeeIdCard'>
+        <h2 id='employeeIdCard'>Status: {status}</h2>
+        <article>Woomy ID: { employeeId }</article>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </section>
+    </>
+  );
 }
